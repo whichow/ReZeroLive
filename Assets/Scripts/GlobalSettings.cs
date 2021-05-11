0f4fde13207c48165c70fa0283e5bd248856c5e8
wuhao <wuhao@orbbec.com>
2021-05-11 20:38:06 +0800

    add voice

--- Assets/Scripts/GlobalSettings.cs
+++ Assets/Scripts/GlobalSettings.cs
@@ -1,5 +1,29 @@
 
+using UnityEngine;
+
 public class GlobalSettings
 {
     public static bool IsShowingPanel = false;
+
+    private static bool playVoice = true;
+    public static bool PlayVoice
+    {
+        get
+        {
+            return playVoice;
+        }
+        set
+        {
+            if(value)
+            {
+                AudioListener.volume = 1;
+                playVoice = true;
+            }
+            else
+            {
+                AudioListener.volume = 0;
+                playVoice = false;
+            }
+        }
+    } 
 }
---
+++ Assets/Scripts/ToggleVoice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleVoice : MonoBehaviour
+{
+    public Text buttonText;
+    private Button toggleButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleButton = GetComponent<Button>();
+        buttonText = GetComponentInChildren<Text>();
+        toggleButton.onClick.AddListener(()=>{
+            GlobalSettings.PlayVoice = !GlobalSettings.PlayVoice;
+            buttonText.text = GlobalSettings.PlayVoice ? "声音开" : "声音关";
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
