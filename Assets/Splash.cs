580293804918dbc244e7416fd79d2da6d5cdcb07
wuhao <wuhao@orbbec.com>
2021-05-20 09:30:49 +0800

    1.0 release

---
+++ Assets/Splash.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        var async = SceneManager.LoadSceneAsync(1);
+        async.allowSceneActivation = false;
+        yield return new WaitForSeconds(3f);
+        while(!async.isDone)
+        {
+            if (async.progress >= 0.9f)
+            {
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
