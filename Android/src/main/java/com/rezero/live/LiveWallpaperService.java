de6d89517b0e61e6daac51a2f9a3a733f5373376
wuhao <wuhao@orbbec.com>
2021-07-06 16:35:23 +0800

    remove android project

--- Android/src/main/java/com/rezero/live/LiveWallpaperService.java
+++
@@ -1,9 +0,0 @@
-package com.rezero.live;
-
-import com.lostpolygon.unity.livewallpaper.UnityWallpaperService;
-
-/**
- * Live Wallpaper Service. Override your stuff here.
- */
-public class LiveWallpaperService extends UnityWallpaperService {
-}
